b5f2ef44ad68e6ec2429dab4b40df8005fc075c7
imurata <ippei.murata.hd@gmail.com>
2025-05-26 18:39:53 +0900

    nocomment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# kong-grafana-loki-tempo
+## How to use
+1. Boot Kong Gateway, Prometheus, Grafana, Loki, Tempo, etc
+```sh
+./run.sh start
+```
+It wrap `docker-compose up` and configure Kong Gateway's Service/Route/Plugin.
+Multiple containers are launched after running the command, and each can be accessed as follows.
+|Service|Host|
+|---|---|
+|Kong Gateway|http://localhost:8002|
+|Prometheus|http://localhost:9090|
+|Grafana|http://localhost:3000|
+|httpbin (via GW)|http://localhost:8000/httpbin|
+
+2. Run API requests
+Send an API Request and record metrics, logs, and tracings by doing the following.
+```sh
+./run.sh test
+```
+
+
---
+++ run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+. env.sh
+COMMAND=$1
+
+if [ ! -x "$(which docker 2>/dev/null)" ]; then
+    echo "Cannot execute docker command."
+    echo "Aborting."
+    exit 1
+fi
+if [ -f "$KONG_LICENSE_FILE" ]; then
+    export KONG_LICENSE_DATA=$(cat $KONG_LICENSE_FILE)
+fi
+
+function all_start()
+{
+    docker-compose up -d 
+    echo "Waiting for Kong Admin API to be ready..."
+    while ! curl -s -o /dev/null -w "%{http_code}" http://localhost:8001/status | grep -q 200; do
+        echo "Kong Admin API is not ready yet. Waiting 5 seconds..."
+        sleep 5
+    done
+    echo "Kong Admin API is ready!"
+    deck gateway sync ./config/deck/sample.yaml
+}
+
+function all_stop()
+{
+    docker-compose down --volumes --remove-orphans
+}
+
+case "$COMMAND" in
+    "stop")
+        all_stop
+        ;;
+    "restart")
+        all_stop
+        all_start
+        ;;
+    "start")
+        all_start
+        ;;
+    "test")
+        curl -i http://localhost:8000/httpbin/anything
+        open http://localhost:3000
+        ;;
+    *)
+        echo "Unknown command: $COMMAND"
+        echo "Usage: $0 {start|stop|restart|test}"
+        exit 1
+        ;;
+esac
